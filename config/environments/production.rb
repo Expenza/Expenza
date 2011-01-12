94c61d1f568cff67f609b588170f5dbb830d1d62
Expenza <expenza.inc@gmail.com>
2011-01-11 21:31:18 -0600

    email send

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,8 @@ Expenza::Application.configure do
 
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_url_options = { :host => 'expenza.heroku.com' }
+
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
