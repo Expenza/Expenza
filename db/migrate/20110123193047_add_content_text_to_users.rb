826cad16e2da527786c994df80182f52905139f3
Expenza <expenza.inc@gmail.com>
2011-01-27 21:58:02 -0600

    mailman2

--- db/migrate/20110123193047_add_content_text_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddContentTextToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :content_text, :string
-  end
-
-  def self.down
-    remove_column :users, :content_text
-  end
-end
